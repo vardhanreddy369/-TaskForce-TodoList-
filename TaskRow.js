51183339522585cfef7472bee1daa78e12ae1e83
SRI VARDHAN REDDY <91843112+vardhanreddy369@users.noreply.github.com>
2023-02-17 11:22:34 +0530

    Add files via upload

---
+++ TaskRow.js
@@ -0,0 +1,16 @@
+export const TaskRow = ({ task, toggleTask }) => {
+  return (
+    <tr>
+      <td className='d-flex justify-content-between'>
+        {task.name}
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={() => {
+            toggleTask(task);
+          }}
+        />
+      </td>
+    </tr>
+  );
+};
